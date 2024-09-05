8d5adbe7e5945f7823cbbb36f73e885c61c5009d
eliasaf-abargel <eliasafabargel@gmail.com>
2024-09-06 02:29:24 +0300

    "add dark mode to quiz app"

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,20 +1,11 @@
-import React, { useState } from "react";
+// import React from 'react'
 import Quiz from "./Components/Quiz/Quiz";
-import "./Quiz.css"; // Import only Quiz-related styles
 
 const App = () => {
-  const [darkMode, setDarkMode] = useState(false);
-
   return (
-    <div className={darkMode ? "dark-mode" : ""}>
-      {/* Button to toggle between light and dark modes */}
-      <button onClick={() => setDarkMode(!darkMode)}>
-        {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
-      </button>
-
-      {/* Render the Quiz component */}
+    <>
       <Quiz />
-    </div>
+    </>
   );
 };
 
